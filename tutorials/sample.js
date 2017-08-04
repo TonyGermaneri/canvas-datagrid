d98ae0a1561e22cfc52d32c1db4879ca75125e49
Tony Germaneri <tgermaneri@fandango.com>
2017-08-04 11:06:44 -0700

    updated samples

--- tutorials/sample.js
+++ tutorials/sample.js
@@ -302,8 +302,8 @@ document.addEventListener('DOMContentLoaded', function () {
             });
             grid.addEventListener('contextmenu', function (e) {
                 e.items.push({
-                    title: 'Asynchronous child context menu item',
-                    items: function (callback) {
+                    title: 'Function based child context menu item',
+                    items: function () {
                         return [{
                             title: 'I was added via a function',
                             click: function () { return; }
@@ -1130,8 +1130,8 @@ document.addEventListener('DOMContentLoaded', function () {
             editor.id = 'e' + i;
             h2.innerHTML = msg[0];
             p.innerHTML = msg[1] || msg[0];
-            form.appendChild(a);
             form.appendChild(h2);
+            form.appendChild(a);
             form.appendChild(p);
             form.appendChild(evaluate);
             form.appendChild(openInFiddle);
