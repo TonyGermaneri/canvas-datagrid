8d659234f1042aafcb646b82aa28e2dcb4c6eb1e
Tony Germaneri <tgermaneri@fandango.com>
2017-08-14 23:26:28 -0700

    added coverage report and build artifacts to gh-pages

---
+++ build/lib/docs.js
@@ -0,0 +1,9 @@
+
+var __cov_g_C8tfl7VcEWnznsTooEWw = (Function('return this'))();
+if (!__cov_g_C8tfl7VcEWnznsTooEWw.__coverage__) { __cov_g_C8tfl7VcEWnznsTooEWw.__coverage__ = {}; }
+__cov_g_C8tfl7VcEWnznsTooEWw = __cov_g_C8tfl7VcEWnznsTooEWw.__coverage__;
+if (!(__cov_g_C8tfl7VcEWnznsTooEWw['/Users/tgermaneri/git/canvas-datagrid/lib/docs.js'])) {
+   __cov_g_C8tfl7VcEWnznsTooEWw['/Users/tgermaneri/git/canvas-datagrid/lib/docs.js'] = {"path":"/Users/tgermaneri/git/canvas-datagrid/lib/docs.js","s":{},"b":{},"f":{},"fnMap":{},"statementMap":{},"branchMap":{}};
+}
+__cov_g_C8tfl7VcEWnznsTooEWw = __cov_g_C8tfl7VcEWnznsTooEWw['/Users/tgermaneri/git/canvas-datagrid/lib/docs.js'];
+
