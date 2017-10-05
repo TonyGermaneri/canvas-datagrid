55c7e5a39cfb4c9671c4884ff62b142b1efc6ad9
Tony Germaneri <tgermaneri@fandango.com>
2017-10-05 13:55:43 -0700

    added react example

---
+++ tutorials/reactExample.js
@@ -0,0 +1,28 @@
+/*jslint browser: true, es6: true*/
+class CanvasDatagrid extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    componentWillReceiveProps() {
+        Object.keys(this.props).forEach(key => {
+            if (grid.attributes[key] !== undefined) {
+                this.grid.attributes[key] = this.props[key];
+            } else if (this.grid[key]) {
+                this.grid[key] = this.props[key];
+            }
+            
+        });
+    }
+    componentWillUnmount() {
+
+    }
+    componentDidMount() {
+        var args = {};
+        Object.keys(this.props).forEach(key => args[key] = this.props[key]);
+        args.parentNode = ReactDOM.findDOMNode(this);
+        this.grid = canvasDatagrid(args);
+    }
+    render() {
+        return React.createElement('div', {}, 'blah');
+    }
+}
