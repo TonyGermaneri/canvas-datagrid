ee949b3a21c45f1c6ad2a543246b35bc9339f8bd
Tony Germaneri <tgermaneri@fandango.com>
2017-11-16 21:42:11 -0800

    added xhr paging demo using http://jservice.io

--- tutorials/xhrPagingDemo.js
+++ tutorials/xhrPagingDemo.js
@@ -1,109 +1,70 @@
 /*jslint browser: true*/
 /*globals canvasDatagrid: false*/
-var data;
+
 function demo() {
     'use strict';
-    var grid, schemaCache, dataCache, searchUrl = window.location.search.substring(3),
-        typeMap = {
-            'text': 'string',
-            'money': 'number',
-            'number': 'number'
-        };
-    function isNoiseData(name) {
-        // get rid of fields that we don't care about
-        return ['sid', 'id', 'position', 'created_at',
-                    'created_meta', 'updated_at',
-                    'updated_meta', 'meta'].indexOf(name) !== -1;
-    }
-    function parseOpenData(openData) {
-        var data, schema = [];
-        openData.meta.view.columns.forEach(function (column) {
-            if (isNoiseData(column.name)) {
-                column.hidden = true;
+    var grid, dataCache, scrollDebounce, loadingText = '\u21BB\u21BB\u21BB\u21BB\u21BB\u21BB\u21BB';
+    function updateLocalCache(data, offset) {
+        var d, x;
+        if (dataCache === undefined) {
+            dataCache = [];
+            dataCache.length = 156800;
+            for (x = 0; x < dataCache.length; x += 1) {
+                dataCache[x] = {
+                    id: x,
+                    loaded: false,
+                    question: loadingText,
+                    answer: loadingText,
+                    value: 0,
+                    airDate: 0
+                };
             }
-            column.type = typeMap[column.dataTypeName] || 'string';
-            if (/full or part-time/i.test(column.name)) {
-                column.enum = [['F', 'F'], ['P', 'P']];
-            }
-            if (/salary or hourly/i.test(column.name)) {
-                column.enum = [['Salary', 'Salary'], ['Hourly', 'Hourly']];
-            }
-            schema.push(column);
-        });
-        data = openData.data.map(function (row) {
-            var r = {};
-            schema.forEach(function (column, index) {
-                r[column.name] = row[index];
-            });
-            return r;
-        });
-        return {
-            data: data,
-            schema: schema
-        };
+            grid.data = dataCache;
+            grid.schema[0].hidden = true;
+            grid.schema[1].hidden = true;
+        }
+        for (x = 0; x < data.length; x += 1) {
+            d = dataCache[x + offset];
+            d.loaded = true;
+            d.question = data[x].question;
+            d.answer = data[x].answer;
+            d.value = data[x].value;
+            d.airDate = data[x].airDate;
+        }
+        grid.draw();
     }
-
-    function loadGrid() {
-        grid = canvasDatagrid({
-            parentNode: document.getElementById('grid'),
-            borderDragBehavior: 'move',
-            allowMovingSelection: true,
-            columnHeaderClickBehavior: 'select',
-            allowFreezingRows: true,
-            allowFreezingColumns: true,
-            allowRowReordering: true,
-            tree: false,
-            debug: false
+    function dataAdapter(offset, rows, callback) {
+        var url, xhr = new XMLHttpRequest();
+        xhr.addEventListener('progress', function () {
+            document.body.style.cursor = 'wait';
         });
-        grid.addEventListener('scroll', function (e) {
-
+        xhr.addEventListener('load', function () {
+            var data = JSON.parse(this.responseText);
+            document.body.style.cursor = 'auto';
+            callback(data, offset, rows);
         });
-        grid.addEventListener('contextmenu', function (e) {
-            e.items.push({
-                title: 'View page source',
-                click: function () { window.open('https://github.com/TonyGermaneri/canvas-datagrid/blob/master/tutorials/demo.html', 'src'); }
-            });
-            e.items.push({
-                title: 'View JS module',
-                click: function () { window.open('https://github.com/TonyGermaneri/canvas-datagrid/blob/master/tutorials/demo.js', 'src'); }
-            });
-            e.items.push({
-                title: 'Go to main canvas-datagrid GitHub page',
-                click: function () { window.open('https://github.com/TonyGermaneri/canvas-datagrid', 'src'); }
-            });
-        });
-    }
-    function loadDataSet(url) {
-        var xhr = new XMLHttpRequest();
-        xhr.addEventListener('progress', function (e) {
-            grid.data = [{ status: 'Loading data: ' + e.loaded + ' of ' + (e.total || 'unknown') + ' bytes...'}];
-        });
-        xhr.addEventListener('load', function (e) {
-            grid.data = [{ status: 'Loading data ' + e.loaded + '...'}];
-            var openData = parseOpenData(JSON.parse(this.responseText));
-            schemaCache = openData.schema;
-            // because this demo does not have an actual paging API it can call, it will simulate it with this data cache.
-            dataCache = openData.data;
-            
-        });
+        url = 'http://jservice.io/api/clues?offset=:offset'
+            .replace(':offset', offset)
+            .replace(':rows', rows);
         xhr.open('GET', url);
         xhr.send();
     }
-    loadGrid();
-    if (searchUrl.length > 3) {
-        // work encoded or not, for lazy people who can't be bothered encoding stuff
-        loadDataSet(/%3A/.test(searchUrl) ? decodeURIComponent(searchUrl) : searchUrl);
-    } else {
-        loadDataSet('https://data.cityofchicago.org/api/views/xzkq-xp2w/rows.json?accessType=DOWNLOAD');
-        // inner join
-        // https://data.cityofchicago.org/api/views/pasx-mnuv/rows.json?accessType=DOWNLOAD
-    }
+    grid = canvasDatagrid({
+        parentNode: document.getElementById('grid')
+    });
+    grid.addEventListener('scroll', function () {
+        clearTimeout(scrollDebounce);
+        scrollDebounce = setTimeout(function () {
+            if (grid.data.filter(function (d, i) {
+                    // grid.scrollIndexRect = {top: 111, right: 5, bottom: 146, left: 0}
+                    return i > grid.scrollIndexRect.top && i < grid.scrollIndexRect.bottom && d.loaded === false;
+                }).length > 0) {
+                var offset = grid.scrollIndexRect.top,
+                    rows = grid.scrollIndexRect.bottom - grid.scrollIndexRect.top;
+                dataAdapter(offset, rows, updateLocalCache);
+            }
+        }, 300);
+    });
+    dataAdapter(0, 100, updateLocalCache);
 }
-if (document.addEventListener) {
-    document.addEventListener('DOMContentLoaded', demo);
-} else {
-    setTimeout(function () {
-        'use strict';
-        demo();
-    }, 500);
-}
+document.addEventListener('DOMContentLoaded', demo);
