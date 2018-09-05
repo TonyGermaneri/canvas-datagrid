99b23b33de6f4f3fd2b1d6efcb8895428b0c75d2
Tony Germaneri <Tony.Germaneri@gmail.com>
2018-09-05 11:38:03 -0700

    fixed issue #164

--- tutorials/developer.js
+++ tutorials/developer.js
@@ -1,18 +1,32 @@
 /*jslint browser: true*/
 /*globals canvasDatagrid: false*/
 /* this file is for developing in a sandbox on a local machine */
+
+
+function getData(r, c, f) {
+    var data = [];
+    for (var x = 0; x < r; x++) {
+        data[x] = {};
+        for (var y = 0; y < c; y++) {
+            data[x][y] = f ? f(x, y) : x + ", " + y
+        }
+    }
+    return data;
+}
+
 function g() {
     'use strict';
     var parentNode = document.getElementById('grid');
     var grid = document.createElement('canvas-datagrid');
+
+
     parentNode.appendChild(grid);
-    grid.data = [
-        {col1: 'foo', col2: 0, col3: 'a'},
-        {col1: 'bar', col2: 1, col3: 'b'},
-        {col1: 'baz', col2: 2, col3: 'c'}
-    ];
-    grid.columnOrder = [2, 1, 0];
-    grid.addEventListener('reorder', function () {
-        grid.columnOrder = [1, 2, 0];
-    });
+    var data = getData(10, 10);
+
+
+    grid.style.width = '300px';
+    grid.style.height = '300px';
+
+    grid.data = data; 
+
 }
