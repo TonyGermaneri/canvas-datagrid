ad3150468285ad9c615e443e189dc170f97499bc
ndrsn <ndrsn@users.noreply.github.com>
2020-07-25 19:50:30 +0000

    deploy: 496a3c8e992d698faa638c5b6b8067392923cd96

--- .github/CONTRIBUTING.md
+++
@@ -1,7 +0,0 @@
-## Responsibility of Contributors
-
-* Pull requests must conform to [JSLINT](http://jslint.com/) [ES5](https://es5.github.io/) and [JSDOC](http://usejsdoc.org/).
-* Declared exceptions to JSLINT are ok.
-* [ES6](http://www.ecma-international.org/ecma-262/6.0/index.html) is OK if enclosed in an eval string.  Code must compile on ES5 browsers.  For now...
-* Pull requests must be [attached to an issue](https://github.com/TonyGermaneri/canvas-datagrid/issues).
-* Pull requests that effect code must pass [tests](https://tonygermaneri.github.io/canvas-datagrid/test/tests.html).
--- build/lib/docs.js
+++
@@ -1,9 +0,0 @@
-
-var __cov_MXp9L2nvRAmbjgg$DYK7uQ = (Function('return this'))();
-if (!__cov_MXp9L2nvRAmbjgg$DYK7uQ.__coverage__) { __cov_MXp9L2nvRAmbjgg$DYK7uQ.__coverage__ = {}; }
-__cov_MXp9L2nvRAmbjgg$DYK7uQ = __cov_MXp9L2nvRAmbjgg$DYK7uQ.__coverage__;
-if (!(__cov_MXp9L2nvRAmbjgg$DYK7uQ['/Users/tony.germaneri/code/canvas-datagrid/lib/docs.js'])) {
-   __cov_MXp9L2nvRAmbjgg$DYK7uQ['/Users/tony.germaneri/code/canvas-datagrid/lib/docs.js'] = {"path":"/Users/tony.germaneri/code/canvas-datagrid/lib/docs.js","s":{},"b":{},"f":{},"fnMap":{},"statementMap":{},"branchMap":{}};
-}
-__cov_MXp9L2nvRAmbjgg$DYK7uQ = __cov_MXp9L2nvRAmbjgg$DYK7uQ['/Users/tony.germaneri/code/canvas-datagrid/lib/docs.js'];
-
