13102e96f1c48f2449012b3ec3f6b9d8f4352f18
Joshua Hemphill <jhemphill@tecnicocorp.com>
2020-11-18 08:14:22 +0000

    Added ESModule dist

---
+++ rollup.config.js
@@ -0,0 +1,23 @@
+import strip from '@rollup/plugin-strip';
+import replace from '@rollup/plugin-replace';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+export default {
+	input: 'lib/main.js',
+	output: {
+	  file: 'dist/canvas-datagrid.module.js',
+	  format: 'es',
+	  sourcemap: true
+	},
+	plugins: [
+	  strip({
+		functions:['window.customElements.define']
+	  }),
+	  replace({
+		'window.require': true,
+		'window.customElements': false
+	  }),
+	  nodeResolve(),
+	  commonjs()
+	]
+  };
