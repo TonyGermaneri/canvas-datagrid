f52b742fc744e641c4f23bb052c4530c26e0c7ad
Joshua Hemphill <jhemphill@tecnicocorp.com>
2020-11-19 01:11:52 +0000

    fixed indentation

--- rollup.config.js
+++ rollup.config.js
@@ -3,21 +3,21 @@ import replace from '@rollup/plugin-replace';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 export default {
-	input: 'lib/main.js',
-	output: {
-	  file: 'dist/canvas-datagrid.module.js',
-	  format: 'es',
-	  sourcemap: true
-	},
-	plugins: [
-	  strip({
-		functions:['window.customElements.define']
-	  }),
-	  replace({
-		'window.require': true,
-		'window.customElements': false
-	  }),
-	  nodeResolve(),
-	  commonjs()
-	]
-  };
+  input: 'lib/main.js',
+  output: {
+    file: 'dist/canvas-datagrid.module.js',
+    format: 'es',
+    sourcemap: true
+  },
+  plugins: [
+    strip({
+    functions:['window.customElements.define']
+    }),
+    replace({
+    'window.require': true,
+    'window.customElements': false
+    }),
+    nodeResolve(),
+    commonjs()
+  ]
+};
