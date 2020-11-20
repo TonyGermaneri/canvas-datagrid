e15326406c1a5e540d258415ea3ed4e7a89b83d9
Joshua Hemphill <jhemphill@tecnicocorp.com>
2020-11-20 04:00:29 +0000

    Migration working, testing needed

--- karma.conf.cjs
+++ karma.conf.cjs
@@ -1,12 +1,9 @@
-const mocha = require('mocha')
-module.exports = function(config) {
+module.exports = function (config) {
   config.set({
-
     browserNoActivityTimeout: 180000,
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
-
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
@@ -15,19 +12,27 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       { pattern: 'test/tests.js', included: true, served: true },
-      { pattern: 'dist/canvas-datagrid.debug.js', included: true, served: true },
+      {
+        pattern: 'dist/canvas-datagrid.debug.js',
+        type: 'js',
+        included: true,
+        served: true,
+      },
+      {
+        pattern: 'dist/canvas-datagrid.module.js',
+        type: 'module',
+        included: true,
+        served: true,
+      },
     ],
-
 
     // list of files / patterns to exclude
-    exclude: [
-    ],
-
+    exclude: [],
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      '**/lib/*.js': ['coverage']
+      '**/lib/*.js': ['coverage'],
     },
 
     failOnFailingTestSuite: true,
@@ -40,32 +45,27 @@ module.exports = function(config) {
     reporters: ['progress', 'coverage'],
 
     coverageReporter: {
-      type : 'json',
-      dir : './build',
+      type: 'json',
+      dir: './build',
       file: 'coverage.json',
     },
 
     // web server port
     port: 9876,
 
-
     // enable / disable colors in the output (reporters and logs)
     colors: true,
-
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
 
-
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
-
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['ChromeHeadless'],
-
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
@@ -73,6 +73,6 @@ module.exports = function(config) {
 
     // Concurrency level
     // how many browser should be started simultaneous
-	concurrency: Infinity,
-  })
-}
+    concurrency: Infinity,
+  });
+};
--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,60 @@
-import strip from '@rollup/plugin-strip';
-import replace from '@rollup/plugin-replace';
+import { babel } from '@rollup/plugin-babel';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
+import { terser } from 'rollup-plugin-terser';
 import commonjs from '@rollup/plugin-commonjs';
-export default {
-	input: 'lib/main.js',
-	output: {
-	  file: 'dist/canvas-datagrid.module.js',
-	  format: 'es',
-	  sourcemap: true
-	},
-	plugins: [
-	  strip({
-		functions:['window.customElements.define']
-	  }),
-	  replace({
-		'window.require': true,
-		'window.customElements': false
-	  }),
-	  nodeResolve(),
-	  commonjs()
-	]
-  };
+import replace from 'rollup-plugin-re';
+import clear from 'rollup-plugin-clear';
+
+const input = 'lib/main.js';
+const fileName = 'canvas-datagrid';
+const name = 'canvasDatagrid';
+
+const babelInput = {
+  babelHelpers: 'bundled',
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        corejs: 3,
+        useBuiltIns: 'entry',
+        modules: false,
+      },
+    ],
+  ],
+};
+
+export default [
+  {
+    input,
+    plugins: [
+      clear({ targets: ['dist'] }),
+      nodeResolve(),
+      replace({
+        defines: {
+          NO_GLOBAL: false,
+        },
+        match: /lib(\/|\\)main/,
+        replace: '',
+      }),
+      commonjs(),
+      babel(babelInput),
+    ],
+    output: {
+      file: `dist/${fileName}.module.js`,
+      plugins: [terser()],
+      sourcemap: true,
+    },
+  },
+  {
+    input,
+    plugins: [nodeResolve(), commonjs(), babel(babelInput)],
+    output: {
+      file: `dist/${fileName}.js`,
+      plugins: [terser()],
+      format: 'umd',
+      name,
+      extend: true,
+      sourcemap: true,
+    },
+  },
+];
