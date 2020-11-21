534fdb876fe6b8f25c35e0c059531dd1b92ea302
Joshua Hemphill <jhemphill@tecnicocorp.com>
2020-11-21 02:06:51 +0000

    fixed ESModule code substitution

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,51 @@
-import strip from '@rollup/plugin-strip';
-import replace from '@rollup/plugin-replace';
+import { babel } from '@rollup/plugin-babel';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
+import { terser } from 'rollup-plugin-terser';
 import commonjs from '@rollup/plugin-commonjs';
+import replace from 'rollup-plugin-re';
+import clear from 'rollup-plugin-clear';
+
+const input = 'lib/main.js';
+const fileName = 'canvas-datagrid';
+
+const babelInput = {
+  babelHelpers: 'bundled',
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        corejs: 3,
+        useBuiltIns: 'entry',
+        modules: false,
+        spec: true,
+        targets: '> 0.25%, not dead',
+      },
+    ],
+  ],
+};
 export default {
-  input: 'lib/main.js',
-  output: {
-    file: 'dist/canvas-datagrid.module.js',
-    format: 'es',
-    sourcemap: true
-  },
+  input,
   plugins: [
-    strip({
-    functions:['window.customElements.define']
-    }),
+    clear({ targets: ['dist'] }),
     replace({
-    'window.require': true,
-    'window.customElements': false
+      patterns: [
+        {
+          match: /lib(\/|\\)main/,
+          test: /(\/\/ #NO_GLOBAL)(.|\r|\n)*\1/,
+          /* export default function canvasDatagrid (args) {
+				  return new Grid(args);
+				}; */
+          replace: ``,
+        },
+      ],
     }),
     nodeResolve(),
-    commonjs()
-  ]
+    commonjs(),
+    babel(babelInput),
+  ],
+  output: {
+    file: `dist/${fileName}.module.js`,
+    plugins: [terser()],
+    sourcemap: true,
+  },
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const webpack = require('webpack');
 
 const productionConfig = {
   mode: 'production',
@@ -22,51 +21,12 @@ const productionConfig = {
 
 };
 
-const productionModuleConfig = {
-	...productionConfig,
-
-	plugins: [
-		new webpack.ProvidePlugin({
-			'window.canvasDatagrid':[path.resolve(path.join(__dirname, 'esFill.js')),'exports','canvasDatagrid'],
-			'window.customElements.define':[path.resolve(path.join(__dirname, 'esFill.js')),'exports','define'],
-			'window.customElements':[path.resolve(path.join(__dirname, 'esFill.js')),'exports','customElements']
-		}),
-	],
-	devtool: 'source-map',
-	module: {
-	  rules: [
-		/* { test: /\.js$/, exclude: /node_modules/,type: 'javascript/esm', use: [{
-			loader:"babel-loader",
-			options: {
-				presets: [['@babel/preset-env',{
-					modules:false,
-					bugfixes: true,
-					spec: true,
-					useBuiltIns: 'usage',
-					corejs: 3,
-					targets: {
-						esmodules: true
-					}
-				}]]
-			}
-		}]} */
-	  ]
-	},
-	experiments: {outputModule: true},
-	target: 'es6',
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'canvas-datagrid.module.js',
-		sourceMapFilename: 'canvas-datagrid.module.map'
-	}
-}
-
 const developmentConfig = {
   ...productionConfig,
   
   mode: 'development',
   devtool: 'source-map',
-
+  
   output: {
     path: path.resolve(__dirname, 'dist'),
     library: 'canvasDatagrid',
@@ -77,4 +37,4 @@ const developmentConfig = {
   },
 };
 
-module.exports = [productionConfig, developmentConfig, productionModuleConfig];
+module.exports = [productionConfig, developmentConfig];
