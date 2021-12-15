eb548cd307bb1cfcbf500394a8d56e3fad25d4e9
Mark de Brauw <mark@debrauw.net>
2021-12-15 18:12:29 +0100

    Resize selected columns simultaneously (#451)
    * Resize selected columns when dragging one of selected columns
    
    * Fix: Resize by bound column index
    
    * Resize selected columns when one of selected columns is double-clicked
    
    Co-authored-by: Mark <mark@mesoica.com>

--- test/resize.js
+++ test/resize.js
@@ -33,6 +33,44 @@ export default function () {
       assertPxColor(grid, 100, 36, c.b, done);
     }, 10);
   });
+  it('Resizes selected columns.', function () {
+    var grid = g({
+      test: this.test,
+      data: smallData(),
+      style: {
+        cellWidth: 50,
+      },
+    });
+    grid.focus();
+    grid.selectColumn(0);
+    grid.selectColumn(1, true);
+
+    const columnSizes = Object.keys(grid.sizes.columns);
+    doAssert(
+      columnSizes.length === 1 && columnSizes[0] === '-1',
+      'No column widths set',
+    );
+
+    mousemove(document.body, 94, 10, grid.canvas);
+    mousedown(grid.canvas, 94, 10);
+    mousemove(document.body, 190, 10, grid.canvas);
+    mouseup(document.body, 190, 10, grid.canvas);
+
+    doAssert(
+      grid.sizes.columns[0] === grid.sizes.columns[1],
+      'Columns have same width',
+    );
+
+    mousemove(document.body, 190, 10, grid.canvas);
+    mousedown(grid.canvas, 190, 10);
+    mousemove(document.body, 94, 10, grid.canvas);
+    mouseup(document.body, 94, 10, grid.canvas);
+
+    doAssert(
+      grid.sizes.columns[0] === 50 && grid.sizes.columns[1] === 50,
+      'Columns have been set back to original width',
+    );
+  });
   it('Resize a column by double clicking a column header.', function (done) {
     var grid = g({
       test: this.test,
@@ -49,6 +87,32 @@ export default function () {
     grid.focus();
     mousemove(document.body, 94, 10, grid.canvas);
     dblclick(grid.canvas, 94, 10);
+  });
+  it('Resize selected columns by double clicking a column header.', function () {
+    var grid = g({
+      test: this.test,
+      data: smallData(),
+      style: {
+        cellWidth: 50,
+      },
+    });
+
+    grid.selectColumn(0);
+    grid.selectColumn(1, true);
+
+    chai.assert.deepStrictEqual(Object.keys(grid.sizes.columns),
+      ['-1'],
+      'No column widths set',
+    );
+
+    mousemove(document.body, 94, 10, grid.canvas);
+    dblclick(grid.canvas, 94, 10);
+
+    chai.assert.deepStrictEqual(Object.keys(grid.sizes.columns), [
+      '0',
+      '1',
+      '-1',
+    ]);
   });
   it('Resize a column from a cell.', function (done) {
     var grid = g({
