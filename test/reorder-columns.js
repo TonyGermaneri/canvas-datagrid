b0bc8a0457d3ef609f51017910ce50730e8ccbdd
Liu Yue <hangxingliu@gmail.com>
2022-01-26 06:56:36 +0800

    Fix unresolved merge conflict

--- test/reorder-columns.js
+++ test/reorder-columns.js
@@ -1,11 +1,7 @@
 import { mouseup, mousedown, mousemove, contextmenu, g } from './util.js';
 
 export default function () {
-<<<<<<< HEAD
-  it('Hide columns by context menu item after reordering', function (done) {
-=======
   it('Hide columns by context menu item after reordering', async function () {
->>>>>>> master
     const data = [{ c1: 'c1', c2: 'c2', c3: 'c3', c4: 'c4', c5: 'c5' }];
     const schema = Object.keys(data[0]).map((name) => ({ name, width: 50 }));
     for (let i = 0; i < 3; i++) data.push(Object.assign({}, data[0]));
@@ -15,10 +11,7 @@ export default function () {
       test: test,
       schema,
       data,
-<<<<<<< HEAD
-=======
       allowRowReordering: true,
->>>>>>> master
       showFilter: false,
     });
     const dnd = (x1, y1, x2, y2) => {
@@ -27,47 +20,6 @@ export default function () {
       mousemove(document.body, x2, y2, grid.canvas);
       mouseup(document.body, x2, y2, grid.canvas);
     };
-<<<<<<< HEAD
-    const delay = (ms = 1) => new Promise((resolve) => setTimeout(resolve, ms));
-    let error;
-    testFlows()
-      .catch((e) => {
-        error = e;
-        console.error(e.message);
-      })
-      .then(() => done(error));
-
-    async function testFlows() {
-      let contextMenuItems = [];
-      grid.addEventListener('contextmenu', function (e) {
-        contextMenuItems = e.items;
-      });
-
-      grid.focus();
-      // c1, c2, c3, c4, c5 => c2, c1, c3, c4, c5
-      dnd(85, 15, 125, 15);
-      await delay();
-
-      contextmenu(grid.canvas, 85, 10);
-      await delay();
-
-      const hideColumnItem = contextMenuItems.find((it) =>
-        it.title.startsWith('Hide '),
-      );
-      if (!hideColumnItem) throw new Error(`No menu item for hidding column`);
-      hideColumnItem.click(new Event('keyup'));
-      await delay(30);
-
-      const cells = getVisibleCells(grid);
-      cells.forEach((it) => {
-        if (it.header.name === 'c2')
-          throw new Error(`There are column c2 after hidding first column`);
-      });
-    }
-  });
-}
-
-=======
     let contextMenuItems = [];
     grid.addEventListener('contextmenu', function (e) {
       contextMenuItems = e.items;
@@ -100,7 +52,6 @@ function delay(ms = 1) {
   return new Promise((resolve) => setTimeout(resolve, ms))
 }
 
->>>>>>> master
 /** @returns {any[]} */
 function getVisibleCells(grid) {
   return grid.visibleCells.filter(
