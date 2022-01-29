f6e6d09e23b430a90f19bc4fafb560c612a50b13
Liu Yue <hangxingliu@gmail.com>
2022-01-30 01:51:50 +0800

    Add util function `mergeHiddenRowRanges` and unit tests.

--- test/unit/index.js
+++ test/unit/index.js
@@ -1,7 +1,9 @@
 'use strict';
 
 import parseClipboardData from './parse-clip-board-data.js';
+import mergeHiddenRowRanges from './merge-hidden-row-ranges.js';
 
 export default function () {
   describe('clipboard', parseClipboardData);
+  describe('mergeHiddenRowRanges', mergeHiddenRowRanges);
 }
