ee474c6df9c38f21d79317ef9b7090301ea45f90
Mark <mark@mesoica.com>
2022-12-31 15:48:14 +0100

    Update footer

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -69,28 +69,11 @@ const config = {
         style: 'dark',
         links: [
           {
-            title: 'Docs',
-            items: [
-              // {
-              //   label: 'Tutorial',
-              //   to: '/intro',
-              // },
-            ],
-          },
-          {
             title: 'Community',
             items: [
               {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
+                label: 'Slack support',
+                href: 'https://canvas-datagrid.slack.com/',
               },
             ],
           },
@@ -99,12 +82,21 @@ const config = {
             items: [
               {
                 label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                href: 'https://github.com/TonyGermaneri/canvas-datagrid',
+              },
+              {
+                href: 'https://www.npmjs.com/package/canvas-datagrid',
+                label: 'npm',
+              },
+              {
+                href: 'https://canvas-datagrid.js.org/',
+                label: 'js.org',
+                position: 'right',
               },
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Canvas Datagrid`,
       },
       prism: {
         theme: lightCodeTheme,
