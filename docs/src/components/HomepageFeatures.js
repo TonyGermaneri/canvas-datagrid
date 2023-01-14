a22e9b6fad4bbab96594ca6537bbb3fc647d071f
Mark <mark@mesoica.com>
2023-01-14 21:21:39 +0100

    Update landing page

--- docs/src/components/HomepageFeatures.js
+++ docs/src/components/HomepageFeatures.js
@@ -8,12 +8,9 @@ const FeatureList = [
     Svg: require('../../static/img/high-performance.svg').default,
     description: (
       <>
-        Canvas Datagrid is built on top of the HTML5 canvas element, this allows
-        for extremely fast rendering of large datasets. Canvas Datagrid is ideal
-        for displaying and manipulating large sets of data, such as financial or
-        scientific data. The library utilizes various performance optimization
-        techniques such as virtual rendering, to ensure smooth scrolling and
-        responsive user interactions.
+        Canvas-datagrid is an extremely fast library for large datasets,
+        particularly for financial or scientific data, and uses performance
+        optimization for smooth scrolling and responsive interactions.
       </>
     ),
   },
@@ -22,10 +19,8 @@ const FeatureList = [
     Svg: require('../../static/img/customizable.svg').default,
     description: (
       <>
-        Canvas Datagrid provides a wide range of customization options, allowing
-        you to easily adjust the look and feel of the grid to match your
-        application's design. This includes options to change the color scheme,
-        font, and cell sizes, as well as advanced customization options such as
+        Canvas-datagrid offers a wide range of customization options to match
+        your application's design, including color scheme, font, cell sizes and
         custom cell renderers and editors.
       </>
     ),
@@ -35,11 +30,9 @@ const FeatureList = [
     Svg: require('../../static/img/interactive.svg').default,
     description: (
       <>
-        Canvas Datagrid provides a wide range of interactive features that allow
-        users to easily navigate, sort and filter the data as well as the
-        ability to select and edit cells. Additionally, the library also
-        provides support for keyboard shortcuts, making it easy for users to
-        perform common actions quickly and efficiently.
+        Canvas-datagrid provides interactive features like navigation, sorting,
+        filtering, cell selection, editing and keyboard shortcuts making it easy
+        for users to perform common actions
       </>
     ),
   },
